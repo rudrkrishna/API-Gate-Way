708afdfffc4ce2148bffcfebb4ad96776329c644
rudrkrishna <Kittu2909@>
2023-07-24 17:14:27 +0530

    Initial commit

---
+++ src/main/java/com/apigateway/apigateway/ApiGateWayApplication.java
@@ -0,0 +1,16 @@
+package com.apigateway.apigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class ApiGateWayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGateWayApplication.class, args);
+	}
+
+}
